12bfdd2d2d4a0bd3499df64defe38b99e8b37344
ackrunalrv <krunaltabiyad7@gmail.com>
2024-11-16 07:00:25 +0530

    edit portfolio

--- js/script.js
+++ js/script.js
@@ -15,3 +15,40 @@ resumeBtns.forEach((btn, idx) => {
         resumeDetails[idx].classList.add('active')
     });
 });
+
+const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right');
+const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left');
+
+let index = 0;
+
+const activaPortfolio = () => {
+    const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
+
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
+}
+
+arrowRight.addEventListener('click', () => {
+    if (index < 4) {
+        index++;
+    }
+    else{
+        index = 5;
+        arrowRight.classList.add('disabled');
+    }
+
+    activaPortfolio();
+});
+
+arrowLeft.addEventListener('click', () => {
+    if (index < 1) {
+        index++;
+        arrowRight.classList.remove('disabled');
+    }
+    else{
+        index = 0;
+        arrowRight.classList.add('disabled');
+    }
+
+    activaPortfolio();
+});
